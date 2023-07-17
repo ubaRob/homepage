16fab2756e1bd0e02c47dcf451da764e92535cf2
Robert Uba <robertuba.dev@gmail.com>
2023-07-17 06:16:39 +0200

    Added js & little css changes

---
+++ main.js
@@ -0,0 +1,6 @@
+//alert('cześć'); to jest funkcja alert. zostawiam ją tu tylko dla zapamiętania
+
+const firstName = 'Rob'
+const age = 33
+
+console.log(`Cześć! Nazywam się ${firstName} i mam ${age} lata :)`);
