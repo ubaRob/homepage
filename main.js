fc5f2813911e563876f2bab2710cd15baba3736c
Robert Uba <robertuba.dev@gmail.com>
2023-08-03 21:06:13 +0200

    DOM & CSS experimental changes

--- main.js
+++ main.js
@@ -7,24 +7,20 @@ console.log(`Cześć! Nazywam się ${firstName} i mam ${age} lata :)`);
 
 const header = document.querySelector('.page-header__heading--js')
 
-const domHeader = document.querySelector('.dom-article__header--js')
-
-const domArticleContent = document.querySelector('.dom-article__content--js')
-
-console.log(domHeader.innerHTML)
-console.log(domArticleContent.innerHTML)
-
-domHeader.innerHTML = "Nagłówek napisany w JS"
-
-domArticleContent.innerHTML = "Ta treść została napisana przy pomocy DOM w JS<br> Pokolorwana również."
-
 console.log(header.innerHTML)
+header.innerHTML = "Robert Uba - strona domowa."
 
-console.log(domHeader.style)
+const domHeader = document.querySelector('.dom-article__header--js')
+domHeader.innerHTML = "Nagłówek stworzony dzięki DOM w JS"
+console.log(domHeader.innerHTML)
 
-domHeader.style.color = 'red';
-domArticleContent.style.color = 'pink';
+const domContentArticle = document.querySelector('.dom-article__content--js')
+domContentArticle.innerHTML = 'Ta treść powstała w JS dzięki DOM'
+console.log(domContentArticle.innerHTML)
 
+//console.log(domHeader.style)
+
+domHeader.style.color = 'pink';
 
 
 
